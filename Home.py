c0e7a5bcb6445632c401f2461cdef5c623f49f7f
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-05-26 13:27:35 +0200

    Home.py

--- Home.py
+++ Home.py
@@ -10,7 +10,7 @@ st.set_page_config(page_title='PKPD Simulation Tools', page_icon='💊', layout=
 st.sidebar.title('HOME')
 st.title('PKPD Simulation Tools')
 st.caption('Version 1.0.0')
-st.write("""1.0.0 is the web application developed by Dinh Long Huynh, a current Master's Student at Uppsala University, Sweden
+st.write("""PKPD Simulation Tools 1.0.0 is the web application developed by Dinh Long Huynh, a current Master's Student at Uppsala University, Sweden
 
 This application helps students and researchers to simulate different scenarios in clinical trials, using the pharmacokinetic one-compartment model.
          
