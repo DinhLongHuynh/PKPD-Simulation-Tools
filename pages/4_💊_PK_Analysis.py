5f09a6ec0e83aa5d490051eb9977c5dee9c4eebf
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-06-09 15:20:27 +0200

    Add files via upload

--- pages/4_💊_PK_Analysis.py
+++ pages/4_💊_PK_Analysis.py
@@ -4,18 +4,22 @@ import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
 from thefuzz import process
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 st.set_page_config(page_title='PK Analysis', page_icon='💊', layout="wide", initial_sidebar_state="auto", menu_items=None)
 st.title("💊 PK Analysis Tools")
-introduction, file_characteristic, visualization, non_compartment, one_compartment = st.tabs(["Introduction",'File Characteristic','Data Visulization',"Non-compartmental Model", "One-compartmental Model"])
+introduction, file_characteristic, visualization, non_compartment, one_compartment = st.tabs(["Introduction",'File Characteristic','Data Visualization',"Non-compartmental Analysis", "One-compartmental Analysis"])
 
 
 
 with introduction:
     st.write('''This page helps users to analyse the clinical trial data. There are two approach in the analysis: 
          
-1) Non-compartmental Analysis: the analysis will base on the discrete data point on the PK profile. Base on that to directly draw the conclusion of several PK parameters.
-2) One-compartmental Analysis: the analysis will fit the observed data to the predefined model and then derived Pk parameters from the model. ''')
+1) Non-compartmental Analysis: the analysis will base on the discrete data points on the PK profile. Base on that to directly draw the conclusion of several PK parameters.
+2) One-compartmental Analysis: the analysis will fit the observed data to the predefined model and then derived PK parameters from the model. ''')
 
     st.subheader('Input data')
     file = st.file_uploader('Import your csv dataset here')
@@ -24,22 +28,19 @@ with introduction:
         df = pd.read_csv(file)
         st.caption('Next step is characterising the data with File Characteristic tab.')
        
-
-   
-       
+    
     
 
 
 with file_characteristic:
+    st.header("File Characteristic")
+    st.caption('Select the column in your file that corresponding to these descriptions')
+    
     if df is not None:
-        st.subheader("File Characteristic")
-        st.caption('Select the column in your file that corresponding to these descriptions')
-
-
         # Use the similarity check to pre-assign columns
         default_id_col = process.extract('ID',df.columns,limit=1)[0][0]
         default_time_col = process.extract('Time',df.columns,limit=1)[0][0]
-        default_conc_col = process.extract('Concentration',df.columns,limit=1)[0][0]
+        default_conc_col = process.extract('Conc',df.columns,limit=1)[0][0]
         default_dose_col = process.extract('Dose',df.columns,limit=1)[0][0]
         
         default_id_index = df.columns.get_loc(default_id_col)
@@ -52,7 +53,7 @@ with file_characteristic:
         # Let user define the columns
         col1, col2 = st.columns(2)
         with col1: 
-            st.write('Fundamental Information')
+            st.write('Compulsory Information')
             ID_col = st.selectbox('Select a column that represent ID',df.columns,index = default_id_index)
             Time_col = st.selectbox('Select a column that represent Time',df.columns,index = default_time_index)
             Concentration_col = st.selectbox('Select a column that represent Concentration',df.columns, index = default_conc_index)
@@ -86,6 +87,7 @@ with file_characteristic:
     else: 
         st.info('You should upload the file first')
         edited_extract_df = None
+
 
 
 
@@ -207,14 +209,153 @@ with visualization:
 
 
 
-with non_compartment: 
-    st.error('The page is still in progress')
-    st.header('Coming Soon')
 
+with non_compartment:
+    
+    def non_compartmental_analysis(df):
+        # Initialize dataframe
+        data = {
+            'ID': [],
+            'Dose':[],
+            'Slope': [],
+            'Number of Lambda Points': [],
+            'R2 Values': [],
+            'AUC_0-last': [],
+            'AUC_last-inf': [],
+            'AUC_0-inf': [],
+            'Half Life': [],
+            'Apparent Clearance': []
+        }
+        
+        for id in df['ID'].unique():
+            df_id = df[df['ID'] == id].dropna()
+            
+            # Skip if less than 3 points 
+            if df_id.shape[0] < 3:
+                st.error(f'ID "{id}" has less than 3 data points. Double check the input data')
+                continue  
+
+            else:
+                # Calculate AUC 0-last
+                auc_0_last = np.trapz(y=df_id['Conc'], x=df_id['Time'])
+            
+                # Find optimal number of lamda points
+                r2_list = []
+                slope_list = []
+                X = df_id[['Time']]
+            
+                for n_points in range(3, df_id.shape[0] + 1):
+                    df_id_point = df_id.iloc[-n_points:, :]
+                    X_points = df_id_point[['Time']]
+                    Y_points = np.log(df_id_point['Conc'] + 0.00001).values.reshape(-1, 1)
+                    model = LinearRegression()
+                    model.fit(X_points, Y_points)
+                    prediction = model.predict(X_points)
+                    r2 = r2_score(y_true=Y_points, y_pred=prediction)
+                    r2_list.append(r2)
+                    slope_list.append(model.coef_[0][0])
+
+            # Skip if r2_list is empty
+                if not r2_list:
+                    continue  
+            
+                else:
+                    r2_max_index = np.argmax(r2_list)
+                    slope = slope_list[r2_max_index]
+                    auc_last_inf = -df_id['Conc'].iloc[-1] / slope
+                    dose = df_id['Dose'].unique()[0]
+                    auc_0_inf = auc_0_last + auc_last_inf
+                    apparent_CL = dose / auc_0_inf
+                    half_life = -np.log(2)/slope
+
+                # Add to data dictionary
+                data['ID'].append(id)
+                data['Dose'].append(dose)
+                data['Slope'].append(-slope)
+                data['Number of Lambda Points'].append(r2_max_index + 3)
+                data['R2 Values'].append(r2_list[r2_max_index])
+                data['AUC_0-last'].append(auc_0_last)
+                data['AUC_last-inf'].append(auc_last_inf)
+                data['AUC_0-inf'].append(auc_0_inf)
+                data['Half Life'].append(half_life)
+                data['Apparent Clearance'].append(apparent_CL)
+
+        # Create DataFrame
+        df_analysis = pd.DataFrame(data)
+        return df_analysis
+
+    def non_compartmental_plots(df_whole_profile,df_lambda_profile):
+        # Create base figure with whole profile
+        fig = px.scatter(df_whole_profile, x='Time', y='log_conc')
+    
+        # Create scatter plot with trendline for lambda points
+        trend_fig = px.scatter(df_lambda_profile, x='Time', y='log_conc', trendline='ols')
+    
+        # Extract the trendline data
+        trendline_data = trend_fig.data[1]  # The trendline is the second trace in the figure
+    
+        # Add lambda profile to the main figure
+        fig.add_trace(go.Scatter(x=df_lambda_profile['Time'], y=df_lambda_profile['log_conc'], mode='markers', marker=dict(color='red'), name='Lambda Points',showlegend=False))
+    
+        # Add the trendline to the main figure
+        fig.add_trace(trendline_data)
+
+        # Modify the axis label
+        fig.update_layout(title=f'ID {id}', xaxis_title='Time', yaxis_title='Log Concentration')
+    
+        # Display the figure
+        st.plotly_chart(fig)
+
+    st.header('Non-Compartmental Analysis')
+    st.write("Non-compartmental analysis (NCA) is a method used in pharmacokinetics to analyze and interpret drug concentration data without assuming a specific compartmental model for the body's drug distribution and elimination processes. Instead of relying on a predetermined biological model, NCA calculates pharmacokinetic (PK) parameters directly from the observed concentration-time data.")
+    st.write("\n")
+    st.write("One of the most important parameters estimated from NCA is the **terminal slope**, also known as the terminal rate constant. To estimate this parameter, the algorithm considers a certain number of last data points, referred to as lambda points. The consideration starts with 3 points, then gradually increase overtime. During that process, a linear regression is performed between the logarithm concentration and time of these points. Subsequently, the optimal number of lambda points is selected, and the corresponding slope is identified as the terminal rate constant.")
+    st.write("\n")
+    st.write("*However, it is important to note that the terminal slope can be either ka or ke, depending on the PK scenario. The further methods to estimate other PK parameters are decribed in the❓Helps page.*")
+    st.write("\n")
+    st.write("\n")
+
+
+
+    st.subheader('Analysis Results')
+    
+    if edited_extract_df is not None:
+        
+        non_compartment_df = non_compartmental_analysis(edited_extract_df)
+        if not non_compartment_df.empty:
+            covariate_df = edited_extract_df.drop(['Time','Conc','Dose'],axis = 1).drop_duplicates(subset='ID')
+            analysis_covariate_df = non_compartment_df.merge(covariate_df, on = 'ID')
+            non_compartment_df_final = st.data_editor(analysis_covariate_df, num_rows="dynamic")
+            
+            #Display the selection profile:
+            plots = st.toggle('Display the individual profiles')
+            counts = non_compartment_df_final['ID'].nunique()
+            if plots:
+                col1, col2 = st.columns(2)
+                for id, lambda_points, count in zip(non_compartment_df_final['ID'],non_compartment_df_final['Number of Lambda Points'],range(counts)):
+                    df_whole_profile = edited_extract_df[edited_extract_df['ID']==id].dropna()
+                    df_lambda_profile = df_whole_profile.iloc[-lambda_points:,:].dropna()
+
+                    df_whole_profile['log_conc'] = np.log(df_whole_profile['Conc'])
+                    df_lambda_profile['log_conc'] = np.log(df_lambda_profile['Conc'])
+
+                    # Plot with the 2 columns layout
+                    if count%2 == 0:
+                        with col1:
+                            non_compartmental_plots(df_whole_profile,df_lambda_profile)
+                    else:
+                        with col2:
+                            non_compartmental_plots(df_whole_profile,df_lambda_profile)
+    
+        else:
+            st.info('For non-compartmental analysis, there should be at least 3 data points for each individuals. Double check your input data.')
+    
+    else:
+        st.info('You should upload the file first')
 
 
 
 
 with one_compartment:
-    st.error('The page is still in progress')
-    st.header('Coming Soon')
+    st.title('One-Compartmental Analysis')
+    st.write('write some introduction here')
--- pages/5_❓_Helps.py
+++ pages/5_❓_Helps.py
@@ -8,6 +8,20 @@ st.write('''This page contains further interpretation of the parameters and vari
 The application provides the common standard units used in clinical trial, i.e. hour for time, mg for dose, mg/L for concentration.
 However, the units can be flexible depends on users' case. In this scenario, users should keep in mind the units when import and interpret the simulation results.''')
 
+st.write("\n")
+st.markdown(
+    """
+    <style>
+    .centered-text {
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<h2 class="centered-text">---------------------💊💊💊---------------------</h2>', unsafe_allow_html=True)
+st.write("\n")
+
 st.header('Combine Dosing Reigmen Simulation PK')
 st.caption('Further description of the parameters and variables used in the simulation:')
 st.write('- **Dose IV (mg):** The data type is **float**. This is the dose of intravenous drug.')
@@ -21,6 +35,19 @@ st.write('- **Absorption Constant (h-1):** The data type is **float**. This is t
 st.write('- **Bioavailability:** The data type is **float**. This is the total bioavailability of drugs in the scale from 0 to 1. For example, if the bioavailability is 85%, the input value should be 0.85.')
 st.write('- **Simulation Range (h):** The data type is **integer**. This is the endpoint of the time range on the simulation plot. For example, if you input 100, the plot will show the simulation within the range between 0 and 100 hour.')
 
+st.write("\n")
+st.markdown(
+    """
+    <style>
+    .centered-text {
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<h2 class="centered-text">---------------------💊💊💊---------------------</h2>', unsafe_allow_html=True)
+st.write("\n")
 
 st.header('PK Simulation')
 st.caption('Further description of the parameters and variables used in the simulation:')
@@ -37,6 +64,19 @@ st.latex(r'Parameter_{i} = Parameter_{population} \times e^{n_{i}}')
 st.write("- **Sigma Residual**: The data type is **float**. This is the standard deviation of the residual distribution. From this distribution, a sample with specific size, which is number of patients is picked, and each residual values will be added to the final concentration with the following formula:")
 st.latex(r'Concentration_{i,final} = Concentration_{i} + Residual_{i}')
 
+st.write("\n")
+st.markdown(
+    """
+    <style>
+    .centered-text {
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<h2 class="centered-text">---------------------💊💊💊---------------------</h2>', unsafe_allow_html=True)
+st.write("\n")
 
 st.header('PD Simulation')
 st.caption('Further description of the parameters and variables used in the simulation:')
@@ -51,3 +91,56 @@ st.write("- **Omega:**: The data type is **float**. Each PD parameter has its co
 st.latex(r'Parameter_{i} = Parameter_{population} \times e^{n_{i}}')
 st.write("- **Sigma Residual**: The data type is **float**. This is the standard deviation of the residual distribution. From this distribution, a sample with specific size, which is number of patients is picked, and each residual values will be added to the final effect with the following formula:")
 st.latex(r'Effect_{i,final} = Effect_{i} + Residual_{i}')
+
+st.write("\n")
+st.markdown(
+    """
+    <style>
+    .centered-text {
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<h2 class="centered-text">---------------------💊💊💊---------------------</h2>', unsafe_allow_html=True)
+st.write("\n")
+
+st.header('PK Analysis')
+st.subheader('File Characteristic')
+st.write('- **Imported file**: The input file format should be a comma-separated value (.csv). After importing the data, the "File Characteristic" page allows users to select the columns in the input file that correspond to the given information.')
+st.write("- **Compulsory Information**: they are the fundamental data that one clinical study must have. If your dataset doesn't have enough compulsory information, you should double check your data, or your study design. ")
+st.write("- **Additional Information**: they are optional. If they are not available, you can leave it blank.")
+st.write("\n")
+st.write("\n")
+
+st.subheader('Non-Compartmental Analysis')
+st.write("Non-compartmental analysis (NCA) is a method used in pharmacokinetics to analyze and interpret drug concentration data without assuming a specific compartmental model for the body's drug distribution and elimination processes. Instead of relying on a predetermined biological model, NCA calculates pharmacokinetic (PK) parameters directly from the observed concentration-time data.")
+st.caption("Below are detail description of the PK parameters derived from the analysis:")
+st.write("- **Slope:** This is the terminal rate constant of drug's PK profile. The unit of the terminal slope is the inverse of the time unit used in the analysis. For example, if the time unit is hours (h), the terminal slope unit is 1/h. To determine the slope, the algorithm tries to make a linear regression between the logarithm of concentration and time (equation below). This regression is carried out using different numbers of last data points, also known as lambda points. The number of lambda points that results in the highest R-squared value during the regression is selected to calculate the slope. However, it is important to note that the terminal slope can be either ka or ke, depending on the PK scenario")
+st.markdown(
+    """
+    <style>
+    .centered-text {
+        text-align: center;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+st.markdown('<div class="centered-text"><i>On the terminal phase:</i></div>', unsafe_allow_html=True)
+st.latex(r'ln(Concentration) = ln(Concentration_{0}) - slope*Time')
+st.write('- **Number of lambda points:** The chosen number of last data points on the PK profile used for linear regression.')
+st.write('- **R2 value:** R2 values of the linear regression between logarithm of concentration and time.')
+st.write('- **AUC_0-last:** The AUC from time point 0 to the last measured time point. This AUC has the unit depends on the concentration and time used in the analysis. For example, if the unit of concentration is mg/L and unit of time is hour, AUC has the unit of mg*h/L. AUC_0-last is calculated by the sum of the trapezoid area created by different data points.')
+st.latex(r'AUC_{0-last} = \sum_{i=2}^{n} \frac{(C_{i} + C_{i-1})*(t_{i} - t_{i-1})}{2}')
+st.write('- **AUC_last-inf:** The AUC from the last measured time point to the infinity. This AUC has the unit depends on the concentration and time used in the analysis. For example, if the unit of concentration is mg/L and unit of time is hour, AUC has the unit of mg*h/L. AUC_last-inf is estimated from the last concentration and the terminal slope, using the following formula.')
+st.latex(r'AUC_{last-inf} = \frac{\text{Concentration}_{\text{last}}}{\text{slope}}')
+st.write('- **AUC_0-inf:** The AUC from time point 0 to the infinity. This AUC has the unit depends on the concentration and time used in the analysis. For example, if the unit of concentration is mg/L and unit of time is hour, AUC has the unit of mg*h/L. AUC_0-inf is the sum of AUC_0-last and AUC_last-inf.')
+st.latex(r'AUC_{0-inf} = \sum_{i=2}^{n} \frac{(C_{i} + C_{i-1})*(t_{i} - t_{i-1})}{2} + \frac{\text{Concentration}_{\text{last}}}{\text{slope}}')
+st.write('- **Apparent Clearance**: This is the drug total apparent clearance of the drug. It has the unit of volume divided by time, therefore the unit depends on the concentration and time unit used in the analysis. Clearance is calculated from AUC_0-inf and the Dose. It is called apparent clearance as it incorporates the drug bioavailability. If you have information about the drug bioavailability, you can calculate the true value of clearance.')
+st.latex(r'CL_{Apparent} = \frac{CL}{F} = \frac{AUC_{0-inf}}{\text{Dose}}')
+st.write('- **Half life**: This is the time it takes for the body to eliminate half of the drug. The half life has the same unit with the time unit used in the analysis.')
+st.latex(r't_{1/2} = \frac{ln(2)}{slope}')
+
+st.subheader('One-Compartmental Analysis')
