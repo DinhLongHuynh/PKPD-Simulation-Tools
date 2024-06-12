87d37761bbf63477f0093bd4926ab61fc0710175
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-06-12 15:39:26 +0200

    Add files via upload

--- Home.py
+++ Home.py
@@ -6,17 +6,21 @@ from scipy.stats import norm
 
 
 # Define the layout of the app
-st.set_page_config(page_title='PKPD Simulation Tools', page_icon='💊', layout="wide", initial_sidebar_state="auto", menu_items=None)
+st.set_page_config(page_title='PKPD SiAn Tools', page_icon='💊', layout="wide", initial_sidebar_state="auto", menu_items=None)
 st.sidebar.title('HOME')
-st.title('PKPD Simulation Tools')
-st.caption('Version 1.1.0')
-st.write("""PKPD Simulation Tools 1.1.0 is the web application developed by Dinh Long Huynh, a current Master's Student at Uppsala University, Sweden
+st.title('PKPD SiAn Tools')
+st.caption('Version 1.2.0')
+st.write("""PKPD SiAn Tools - PKPD Simulation and Analysis Tools 1.2.0 is the web application developed by Dinh Long Huynh, a current Master's Student at Uppsala University, Sweden.
 
-This application helps students and researchers to simulate different scenarios in clinical trials, using the pharmacokinetic one-compartment model.
-         
-With the 1.1.0 version, the application was updated with the new feature 💊 PK Analysis, which can take input from users, characterize it and generate several visualization plots.
-The "Non-compartmental Analysis" and "One-compartmental analysis" are still in progress and will come soon.""")
+This application helps students and researchers to simulate and analyze different scenarios in clinical trials. The simulation uses a one-compartmental model and the analysis can be conducted based on both non-compartment and one-compartment models. The underlying mechanism of the simulation and analysis is in the *❓Helps* page. You can click the button below to see the mechanism.""")
+st.link_button("❓Helps", "https://pkpd-sian-tools.streamlit.app/Helps")
+st.write("""With the 1.2.0 version, the 💊 PK Analysis tab was updated with the new features of Non-compartmental Analysis and One-compartmental Analysis, which can take input from users and generate the PK parameters.
+The ongoing 1.2.1 version will be updated soon, which incorporates the AI data cleaning tools, which can draw several error warnings for the imported data. """)
+
+
 st.sidebar.success('Select a page above')
+
+
 
 
 
