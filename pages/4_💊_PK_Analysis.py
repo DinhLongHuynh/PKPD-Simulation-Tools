4798c4730f5b9f499c841c50974272b5af52a2dd
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-06-13 16:34:07 +0700

    4_💊_PK_Analysis.py

--- pages/4_💊_PK_Analysis.py
+++ pages/4_💊_PK_Analysis.py
@@ -9,7 +9,6 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error
 from scipy.optimize import curve_fit
 from scipy.integrate import quad
-import os
 
 # Page setup
 st.set_page_config(page_title='PK Analysis', page_icon='💊', layout="wide", initial_sidebar_state="auto", menu_items=None)
@@ -48,9 +47,9 @@ with introduction:
     if uploaded_file is not None:
         file = uploaded_file
     elif iv_drug_file:
-        file = os.path.join('..', 'testdata', 'Phase_I_iv_drug.csv')
+        file = '../testdata/Phase_I_iv_drug.csv'
     elif im_drug_file:
-        file = os.path.join('..', 'testdata', 'Phase_I_im_drug.csv')
+        file = '../testdata/Phase_I_im_drug.csv'
 
 # Read and store the data in session state
     if file is not None:
