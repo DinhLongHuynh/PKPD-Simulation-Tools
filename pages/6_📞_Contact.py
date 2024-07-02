ccdf671e7af639efdfe226ab25b911e688e9ddf0
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-07-02 09:41:09 +0700

    6_📞_Contact.py

--- pages/6_📞_Contact.py
+++ pages/6_📞_Contact.py
@@ -4,8 +4,9 @@ st.set_page_config(page_title='Contact', page_icon='📞', layout="wide", initia
 st.title('📞 Contact Information')
 
 # Contact Information
-st.write("**Mr.** Dinh Long Huynh - Master's Student at Uppsala University")
+st.write('**Dinh Long Huynh** - Master's Student at Uppsala University")
 st.write('**Email:** dinhlong240600@gmail.com')
 st.write('**Phone:** (+46) 735 671 651 - (+84) 938 548 597')
 st.write('**Linkedin:** https://www.linkedin.com/in/dinh-long-huynh-996193241/')
+st.write('**GitHub:** https://github.com/DinhLongHuynh')
 st.write('*Copyright © 2024 Dinh Long Huynh*')
