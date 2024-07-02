ae7baadedc265e338494a8af11ed31177716dd69
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-07-02 21:54:30 +0700

    Add files via upload

--- pages/6_📞_Contact.py
+++ pages/6_📞_Contact.py
@@ -1,12 +1,14 @@
 import streamlit as st
+from chatbox import chatbox
 
 st.set_page_config(page_title='Contact', page_icon='📞', layout="wide", initial_sidebar_state="auto", menu_items=None)
 st.title('📞 Contact Information')
+chatbox()
 
 # Contact Information
-st.write("**Author:** Dinh Long Huynh - Master's Student at Uppsala University")
-st.write('**Email:** dinhlong240600@gmail.com')
+st.write("**Mr.** Dinh Long Huynh - Master's Student at Uppsala University")
 st.write('**Phone:** (+46) 735 671 651 - (+84) 938 548 597')
+st.write('**Email:** dinhlong240600@gmail.com')
 st.write('**Linkedin:** https://www.linkedin.com/in/dinh-long-huynh-996193241/')
 st.write('**GitHub:** https://github.com/DinhLongHuynh')
 st.write('*Copyright © 2024 Dinh Long Huynh*')
