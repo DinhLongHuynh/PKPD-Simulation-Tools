2aa5077ab14c701c2f4c675473768674fd8b08b6
Dinh Long Huynh <151563450+DinhLongHuynh@users.noreply.github.com>
2024-07-16 16:59:54 +0700

    Add files via upload

--- Home.py
+++ Home.py
@@ -11,15 +11,15 @@ st.sidebar.title('HOME')
 
 # App introduction
 st.title('💊 PKPD SiAn Tools')
-st.caption('Version 1.4.0')
-st.write("""PKPD SiAn Tools - PKPD Simulation and Analysis Tools 1.4.0 is the web application developed by Dinh Long Huynh, a current Master's Student at Uppsala University, Sweden.
+st.caption('Version 2.0.0')
+st.write("""PKPD SiAn Tools - PKPD Simulation and Analysis Tools 2.0.0 is the web application developed by Dinh Long Huynh, a current Master's Student at Uppsala University, Sweden.
 
 This application helps students and researchers to simulate and analyze different scenarios in clinical trials. The simulation uses a one-compartmental model and the analysis can be conducted based on both non-compartment and one-compartment models. The underlying mechanism of the simulation and analysis is in the *❓Helps* page. You can click the button below to see the mechanism.""")
 st.link_button("❓Helps", "https://pkpd-sian-tools.streamlit.app/Helps")
-st.write("""In version 1.4.0, **Multiple Compartmental Model** is included, which allows to simulate more complex scenarios with consideration of one or more peripheral compartments.
+st.write("""In version 2.0.0, the Simulation Tools are re-organized into PK Simulation and Population PK Simulation. The PK Simulation allows the flexible dosing regimen, while the Population PK Simulation now is available for single dose only.
+This version also uses the generalized API in source code by the package pkpd_sian included in the same Repository.
          
-The ongoing 1.5.0 version will be updated soon, which incorporates two new features, including automatical data cleaning tools, which helps draw several error warnings for the imported data and One-compartmental Analysis for multiple dosing regimen.""")
-           
+The ongoing 2.1.0 version will be updated soon, which incorporates Physologically Based Pharmacokinetic Models. """)
 
 
 # Sidebar options
@@ -48,6 +48,8 @@ if update_history:
              
 - **Version 1.3.1:** All error messages were written as clear instructions so that users can easily fix simulation errors.
              
-- **Version 1.4.0:** Multiple Compartmental Model is included, which allows to simulate more complex scenarios with consideration of one or more peripheral compartments.''')
+- **Version 1.4.0:** Multiple Compartmental Model is included, which allows to simulate more complex scenarios with consideration of one or more peripheral compartments.
+             
+- **Version 2.0.0:** Simulation Tools are re-organized into PK Simulation and Population PK Simulation. The PK Simulation allows the flexible dosing regimen, while the Population PK Simulation now is available for single dose only. This version also uses the generalized API in source code by the package pkpd_sian included in the same Repository.''')
 
 
